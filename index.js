47484393f512fa81538314959e4794ddbb6ced8e
darah98 <darashalabi@gmail.com>
2020-05-28 16:19:37 +0300

    help function fixed

--- index.js
+++ index.js
@@ -23,9 +23,17 @@ function info() {
   console.log(`
   ERR: INVALID INPUT
 
-  Note App USAGE: node index.js -a <note body>
-  -a: the action to be perfomed (replacements: --a | --add)
+  Note App USAGE: 
+  +To add new note: node index.js --add <note body> || --category <category name>
+  --add replacements: --a | -a
   <note body>: replace with the note between single quotes ''
+  <category name>: replace with the wanted catrgory
+  +To delete an existing note: node index.js --delete <note ID>
+  --delete replacements: --d | -d
+  <note ID>: replace with the id displayed after adding 
+  OR you can use list to see the ID
+  +To list notes: node index.js --list || --category <category name>
+  --list replacements: --l | -l
   `);
   process.exit();
 }
