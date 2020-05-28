b2ab06e6c442cb6fcc0bd19d0a432691f5afe628
darah98 <darashalabi@gmail.com>
2020-05-28 16:07:14 +0300

    lab4 done

--- __test__/supergoose.test.js
+++ __test__/supergoose.test.js
@@ -3,7 +3,7 @@ require('@code-fellows/supergoose');
 const noteIns = require('../lib/model/notes-collection.js');
 
 describe('noteIns', () => {
-  it('creates() a new note obj', () => {
+  it('creates a new note object', () => {
     const newNote = {
       action: '--add',
       payload: 'important note',
@@ -13,6 +13,21 @@ describe('noteIns', () => {
       Object.keys(created).forEach((key) => {
         expect(created[key]).toContain('_id');
       });
+    });
+  });
+  it('reads what is in the database', () => {
+    const newNote = {
+      action: '--add',
+      payload: 'important note',
+      category: 'important',
+    };
+    const lAction = {
+      action: '--list',
+      category: 'important',
+    };
+    noteIns.create(newNote).then((listArr) => {
+      noteIns.read(lAction);
+      expect(listArr.length).toBeTruthy;
     });
   });
 });
